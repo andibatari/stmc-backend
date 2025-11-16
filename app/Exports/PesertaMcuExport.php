c6b75c2f49fa9a53223ccfdafdc6583c3b22814d
andibatari <andibatari2311@gmail.com>
2025-11-16 21:45:46 +0800

    Implementasi fungsi ekspor data Karyawan, Lingkungan, dan MCU

---
+++ app/Exports/PesertaMcuExport.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\PesertaMcu;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class PesertaMcuExport implements FromCollection, WithHeadings, WithMapping
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        // Ambil data dari tabel peserta_mcus
+        // Filter di mana 'karyawan_id' adalah null, karena ini menandakan pasien non-PTST
+        // Load relasi yang diperlukan (jika ada, sesuai skema tabel)
+        return PesertaMcu::whereNull('karyawan_id')
+                         ->with(['provinsi', 'kabupaten', 'kecamatan'])
+                         ->get();
+    }
+
+    /**
+     * @param mixed $pesertaMcu
+     * @return array
+     */
+    public function map($pesertaMcu): array
+    {
+        // Peta data dari model PesertaMcu ke baris Excel
+        return [
+            $pesertaMcu->id,
+            $pesertaMcu->no_sap,
+            $pesertaMcu->nik_pasien,
+            $pesertaMcu->nama_lengkap,
+            $pesertaMcu->jenis_kelamin,
+            $pesertaMcu->tempat_lahir,
+            $pesertaMcu->tanggal_lahir,
+            $pesertaMcu->umur,
+            $pesertaMcu->golongan_darah,
+            $pesertaMcu->pendidikan,
+            $pesertaMcu->pekerjaan,
+            $pesertaMcu->perusahaan_asal,
+            $pesertaMcu->agama,
+            $pesertaMcu->alamat,
+            $pesertaMcu->no_hp,
+            $pesertaMcu->email,
+            $pesertaMcu->provinsi->nama_provinsi ?? 'N/A',
+            $pesertaMcu->kabupaten->nama_kabupaten ?? 'N/A',
+            $pesertaMcu->kecamatan->nama_kecamatan ?? 'N/A',
+        ];
+    }
+    
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        // Tentukan nama-nama kolom (header) untuk file Excel
+        return [
+            'ID',
+            'SAP',
+            'NIK Pasien',
+            'Nama Lengkap',
+            'Jenis Kelamin',
+            'Tempat Lahir',
+            'Tanggal Lahir',
+            'Umur',
+            'Golongan Darah',
+            'Pendidikan',
+            'Pekerjaan',
+            'Perusahaan Asal',
+            'Agama',
+            'Alamat',
+            'No. HP',
+            'Email',
+            'Provinsi',
+            'Kabupaten',
+            'Kecamatan',
+        ];
+    }
+}
