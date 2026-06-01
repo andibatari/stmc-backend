da8aeb264ee8980fcc9e87b4de3dd5a8270c104d
andibatari <andibatari2311@gmail.com>
2026-06-01 18:10:52 +0800

    Add FCMService

---
+++ app/Services/FCMService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Google\Auth\Credentials\ServiceAccountCredentials;
+
+class FCMService
+{
+    public static function sendPushNotification($fcmToken, $title, $body)
+    {
+        try {
+            $credentialsPath = storage_path('app/firebase_credentials.json');
+            
+            if (!file_exists($credentialsPath)) {
+                Log::error("FCMService: File kredensial tidak ditemukan di " . $credentialsPath);
+                return false;
+            }
+
+            // Membaca Project ID dari file JSON
+            $json = json_decode(file_get_contents($credentialsPath), true);
+            $projectId = $json['project_id'];
+
+            // Menggunakan Laravel Http Client untuk mengirim ke Firebase HTTP v1 API
+            // (Sederhana tanpa library tambahan)
+            $response = Http::withToken(self::getAccessToken($credentialsPath))
+                ->post("https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send", [
+                    'message' => [
+                        'token' => $fcmToken,
+                        'notification' => [
+                            'title' => $title,
+                            'body' => $body,
+                        ],
+                        'android' => [
+                            'priority' => 'high',
+                        ],
+                    ],
+                ]);
+
+            if ($response->successful()) {
+                Log::info("FCMService: Berhasil dikirim ke " . substr($fcmToken, 0, 10));
+                return true;
+            } else {
+                Log::error("FCMService Error: " . $response->body());
+                return false;
+            }
+
+        } catch (\Exception $e) {
+            Log::error("FCMService Exception: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    private static function getAccessToken($credentialsPath)
+    {
+        // Fungsi sederhana untuk mendapatkan token akses dari Google
+        $scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
+        $credentials = new ServiceAccountCredentials($scopes, $credentialsPath);
+        return $credentials->fetchAuthToken()['access_token'];
+    }
+}
